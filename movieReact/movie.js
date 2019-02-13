9d3b6a2cf6d498073f1a3630718b02ab9110e81a
Robert Bunch <robertbunch@Roberts-MacBook-Pro.local>
2019-02-13 15:25:17 -0500

    Add poster images

--- movieReact/movie.js
+++ movieReact/movie.js
@@ -1,3 +1,12 @@
+// NOT EVERYTHING HAS TO BE A CLASS
+function Poster(props){
+    return(
+        <div className="col s4 m3">
+            <img src={props.image} />
+        </div>
+    )
+}
+
 class App extends React.Component{
     constructor(){
         super();
@@ -9,26 +18,41 @@ class App extends React.Component{
     // componentDidMount is special. react calls it. we dont
     componentDidMount(){
         const url = "https://api.themoviedb.org/3/movie/now_playing?api_key=fec8b5ab27b292a68294261bb21b04a5";
+        
+        // fetch is a replacement for $.getJSON/$.ajax/axios
         fetch(url)
-        .then(function(response) {
+        .then((response)=>{
           return response.json();
         })
-        .then(function(myJson) {
-          console.log((myJson));
+        .then((myJson)=>{
+            const results = myJson.results;
+            console.log(results)
+            this.setState({
+                moviesToShow: results
+            });
         });
 
         console.log("Checking... yes! It's mounted");
-        this.setState({
-            moviesToShow: [1]
-        })
+
     }
 
     // What method do we HAVE to have?
     // REnder is special. react uses it, we dont
     render(){
-        console.log("Rendering...");
+        const imagePath = "http://image.tmdb.org/t/p/w300";
+        let movieList = this.state.moviesToShow.map((movie)=>{
+            const fullImagePath = imagePath + movie.poster_path;
+            return(<Poster image={fullImagePath} />)
+        })
+        console.log(this.state.moviesToShow);
         return(
-            <h1>Sanity Check</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col s12">
+                        {movieList}
+                    </div>
+                </div>
+            </div>
         )
     }
 }
